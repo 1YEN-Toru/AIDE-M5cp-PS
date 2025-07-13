0c274fb01482440e4a72913080a5ce4f112e0cbb
1YEN-Toru <137962771+1YEN-Toru@users.noreply.github.com>
2025-07-13 22:33:39 +0900

    update

--- M5cp-PS/src/m5cp_jpn_font.cpp
+++ M5cp-PS/src/m5cp_jpn_font.cpp
@@ -9,4 +9,9 @@
 
 // This file is empty dummy, use script/m5cp_jpn_font.olv
 // See: http://hello.world.coocan.jp/ARDUINO23/arduino23_5.html#LIBRARY
+#warning	"M5cp-PS lib: m5cp_jpn_font.cpp is empty dummy."
 
+
+extern	const	unsigned	short	tft_prn_font_kz[]=
+{
+};
--- M5cp-PS/src/m5cp_jpn_u2s.cpp
+++ M5cp-PS/src/m5cp_jpn_u2s.cpp
@@ -7,6 +7,17 @@
 //
 
 
+#include	<m5cp_ps.h>
+
+
 // This file is empty dummy, use script/m5cp_jpn_u2s.olv
 // See: http://hello.world.coocan.jp/ARDUINO23/arduino23_5.html#LIBRARY
+#warning	"M5cp-PS lib: m5cp_jpn_u2s.cpp is empty dummy."
 
+
+const	uint16	unsj_hash_tab[]=		// jpn: hash table
+{
+};
+const	uint8	unsj_lkup_tab[]=		// jpn: look up table
+{
+};
