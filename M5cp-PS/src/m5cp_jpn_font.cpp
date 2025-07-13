2383ee73c2b5a9cb887eb494024ac7766a64a6f3
1YEN-Toru <137962771+1YEN-Toru@users.noreply.github.com>
2025-07-13 18:36:09 +0900

    create

---
+++ M5cp-PS/src/m5cp_jpn_font.cpp
@@ -0,0 +1,12 @@
+//
+//	M5StickC Plus postscript library
+//		Japanese font module
+//		(c) 2022	1YEN Toru
+//
+//	Generated using script: m5cp_jpn_font.olv
+//
+
+
+// This file is empty dummy, use script/m5cp_jpn_font.olv
+// See: http://hello.world.coocan.jp/ARDUINO23/arduino23_5.html#LIBRARY
+
